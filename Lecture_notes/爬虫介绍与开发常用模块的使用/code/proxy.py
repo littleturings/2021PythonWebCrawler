d1862d21ab9344f76852ff0ead4ea717ad3b3e48
Misaka13514 <Misaka13514@gmail.com>
2021-02-01 23:36:13 +0800

    codestyle&fix(demo code): format code & fix bug

--- Lecture_notes/爬虫介绍与开发常用模块的使用/code/proxy.py
+++ Lecture_notes/爬虫介绍与开发常用模块的使用/code/proxy.py
@@ -1,13 +1,13 @@
-from urllib.request import Request,build_opener
+from urllib.request import Request, build_opener
 from fake_useragent import UserAgent
 from urllib.request import ProxyHandler
 
-url = "http://httpbin.org/get"
-headers = {"UserAgent":UserAgent().chrome}
-req = Request(url,headers=headers)
 
-handler = ProxyHandler({"http":"110.243.27.121:9999"})
-#handler = ProxyHandler({"http":"name:password@ip:port"})
+url = "http://httpbin.org/get"
+headers = {"User-Agent": UserAgent().chrome}
+req = Request(url, headers=headers)
+
+handler = ProxyHandler({"<type>": """(<name>:<password>@)<ip>:<port>"""})
 
 opener = build_opener(handler)
 resp = opener.open(req)
