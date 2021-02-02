0b69fda6486fe7f69f9b975e07c5ece649eeacea
Misaka13514 <Misaka13514@gmail.com>
2021-02-02 14:40:15 +0800

    codestyle(demo code): format code

--- Lecture_notes/数据提取与验证码的识别/code/xpath_test.py
+++ Lecture_notes/数据提取与验证码的识别/code/xpath_test.py
@@ -4,13 +4,13 @@ from lxml import etree
 
 url = "https://www.qidian.com/rank/yuepiao"
 
-header = {"User-Agent":UserAgent().chrome}
-resp = requests.get(url,headers=header)
-#print(resp.text)
+header = {"User-Agent": UserAgent().chrome}
+resp = requests.get(url, headers=header)
+# print(resp.text)
 
 e = etree.HTML(resp.text)
 names = e.xpath('//div[@class="book-mid-info"]/h4/a/text()')
 authors = e.xpath("//p[@class='author']/a[1]/text()")
 
-for name, author in zip(names,authors):
-    print(name,":",author)
+for name, author in zip(names, authors):
+    print(name, ":", author)
--- Lecture_notes/数据提取与验证码的识别/code/段子提取.py
+++ Lecture_notes/数据提取与验证码的识别/code/段子提取.py
@@ -2,11 +2,13 @@ import requests
 from fake_useragent import UserAgent
 import re
 
+
 url = 'https://www.qiushibaike.com/text/'
 headers = {"User-Agent": UserAgent().chrome}
 resp = requests.get(url, headers=headers)
-#print(resp.text)
+# print(resp.text)
+
 contents = re.findall(r'<div class="content">\s*<span>\s*(.+)', resp.text)
-with open('duanzi.txt','a',encoding='utf-8') as f:
+with open('duanzi.txt', 'a', encoding='utf-8') as f:
     for info in contents:
         f.write(info+"\n\n")
