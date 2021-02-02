9fa1cbe330af53119136a1f4458c6324d2c45de1
Orash-Shi <66580467+Orash-Shi@users.noreply.github.com>
2021-02-02 12:04:38 +0800

    added code

---
+++ Lecture_notes/数据提取与验证码的识别/code/段子提取.py
@@ -0,0 +1,12 @@
+import requests
+from fake_useragent import UserAgent
+import re
+
+url = 'https://www.qiushibaike.com/text/'
+headers = {"User-Agent": UserAgent().chrome}
+resp = requests.get(url, headers=headers)
+#print(resp.text)
+contents = re.findall(r'<div class="content">\s*<span>\s*(.+)', resp.text)
+with open('duanzi.txt','a',encoding='utf-8') as f:
+    for info in contents:
+        f.write(info+"\n\n")
